c4c2097ee6db0ece133bb20f402e9a70182b56bd
Colt156 <coltondallas2010@gmail.com>
2025-08-29 11:15:37 -0500

    Update todo.py

--- todo.py
+++ todo.py
@@ -2,6 +2,7 @@
 
 # Step 1: Start with an empty list to hold tasks
 tasks = []
+completedtasks= []
 
 # Step 2: Add a task
 def add_task(task):
@@ -13,11 +14,20 @@ def view_tasks():
         print(f"{i}. {task}")
 # Step 4: Delete a task
 def delete_tasks():
-    
+    Deleted_Tasks=input("Do you want to remove any tasks(y/n)")
+    if Deleted_Tasks=="y":
+        numtask=int(input("Which task(ex. 1,2,ect.)"))
+        print(f"Deleted {tasks[numtask-1]}")
+        tasks.pop(numtask-1)
 # Step 5: Mark task complete
+def mark_complete():
+    Completed_Tasks=input("Have you completed any tasks(y/n)")
+    if Completed_Tasks=="y":
+        numtask=int(input("Which task(ex. 1,2,ect.)"))
+        completedtasks.append(numtask-1)
 
 
-# Step 6: Save/load tasks (extra stretch for today)
+
 
 
 # Demo flow (you can run this file directly: python todo.py)
@@ -25,6 +35,6 @@ if __name__ == "__main__":
     add_task("Finish Cyber 201 assignment")
     add_task("Push code to GitHub")
     view_tasks()
-    #mark_complete(0)
+    delete_tasks()
+    mark_complete()
     view_tasks()
-    #save_tasks()
